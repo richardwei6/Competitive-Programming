cfdfb1a98d534c3c50ef8b6e17baaf12fb89416e
richy6 <richardwei6@gmail.com>
2020-04-23 14:16:45 -0700

    Restructured folders

--- VISUAL STUDIO/USACO Projects/skidesign.cpp
+++ VISUAL STUDIO/USACO Projects/skidesign.cpp
@@ -13,39 +13,7 @@ LANG: C++
 #include <set>
 #include <unordered_set>
 using namespace std;
-
-
-int largestDiff(vector<int> a) {
-	int minH = 100, maxH = 0;
-	for (int i : a) {
-		minH = min(minH, i);
-		maxH = max(maxH, i);
-	}
-	return maxH - minH;
-}
-
-int largestHillpos(vector<int> a) {
-	int maxH = -1, p = -1;
-	for (int i = 0; i < a.size(); i++) {
-		if (a[i] > maxH) {
-			maxH = a[i];
-			p = i;
-		}
-	}
-	return p;
-}
-
-int smallestHillpos(vector<int> a) {
-	int minH = 101, p = -1;
-	for (int i = 0; i < a.size(); i++) {
-		if (a[i] < minH) {
-			minH = a[i];
-			p = i;
-		}
-	}
-	return p;
-}
-
+/*
 int main() {
 	ios_base::sync_with_stdio(false);
 	ofstream fout("skidesign.out");
@@ -59,34 +27,22 @@ int main() {
 		h.push_back(n);
 	}
 
-	vector<int> c(h.size(), 0);
+	long long minCost = 9999999;
 
-	for (int alt = 0; largestDiff(h) > 17; alt++) { // alternating int
-		// subtract 1 unit from largest hill and add 1 unit to smallest hill
-
-		int p;
-		if (alt % 2 == 0) {
-			p = largestHillpos(h);
-			h[p]--; // remove units
-			c[p]++; // cost
+	for (int i = 17; i <= 100; i++) {
+		long long currentCost = 0;
+		for (int n : h) {
+			if (n > i || n < i - 17) { // not in range
+				int j = min(abs(n - (i - 17)), abs(n - i));
+				currentCost += j*j;
+			}
 		}
-		else {
-			p = smallestHillpos(h);
-			h[p]++; // add units
-			c[p]++; // cost
-		}
+		minCost = min(currentCost, minCost);
 	}
-
-	int cost = 0; // calculate cost
-	for (int i : c) {
-		cost += i * i;
-	}
-
-	if (cost > 22946) {
-		cost = 22946;
-	}
-
-	fout << cost << endl;
+	
+	fout << minCost << endl;
 
 	return 0;
 }
+
+*/
